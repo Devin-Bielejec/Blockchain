d5b03aeb56a4dfcc7d716093d77a6367adfa8ced
devin-bielejec <devin.bieleje@gmail.com>
2020-01-30 12:59:07 -0500

    last_block endpoint

--- basic_block_gp/blockchain.py
+++ basic_block_gp/blockchain.py
@@ -5,13 +5,14 @@ from uuid import uuid4
 
 from flask import Flask, jsonify, request
 
+DIFFICULTY = 6
 
 class Blockchain(object):
     def __init__(self):
         self.chain = []
         self.current_transactions = []
 
-        # Create the genesis block
+        # Create the genesis block, 1 is very different from all the other data
         self.new_block(previous_hash=1, proof=100)
 
     def new_block(self, proof, previous_hash=None):
@@ -31,13 +32,19 @@ class Blockchain(object):
         """
 
         block = {
-            # TODO
+            "index": len(self.chain) + 1,
+            "timestamp": time(),
+            "transactions": self.current_transactions,
+            "proof": proof,
+            "previous_hash": previous_hash or self.hash(self.chain-1)
         }
 
         # Reset the current list of transactions
+        self.current_transactions = []
         # Append the chain to the block
+        self.chain.append(block)
         # Return the new block
-        pass
+        return block
 
     def hash(self, block):
         """
@@ -55,9 +62,11 @@ class Blockchain(object):
         # We must make sure that the Dictionary is Ordered,
         # or we'll have inconsistent hashes
 
-        # TODO: Create the block_string
+        # TODO: Create the block_string, sort keys in python3.7 sort automatically, but we could be getting old data
+        block_string = json.dumps(block, sort_keys=True).encode()
 
-        # TODO: Hash this string using sha256
+        # TODO: Hash this string using sha256, requires byte object not actual string
+        hash = hashlib.sha256(block_string).hexdigest()
 
         # By itself, the sha256 function returns the hash in a raw string
         # that will likely include escaped characters.
@@ -66,7 +75,7 @@ class Blockchain(object):
         # easier to work with and understand
 
         # TODO: Return the hashed block string in hexadecimal format
-        pass
+        return hash
 
     @property
     def last_block(self):
@@ -81,8 +90,12 @@ class Blockchain(object):
         :return: A valid proof for the provided block
         """
         # TODO
-        pass
-        # return proof
+        block_string = json.dumps(self.last_block, sort_keys=True)
+        proof = 0
+        while self.valid_proof(block_string, proof) is False:
+            proof += 1
+
+        return proof
 
     @staticmethod
     def valid_proof(block_string, proof):
@@ -97,8 +110,10 @@ class Blockchain(object):
         :return: True if the resulting hash is a valid proof, False otherwise
         """
         # TODO
-        pass
-        # return True or False
+        guess = f"{block_string}{proof}".encode()
+        guess_hash = hashlib.sha256(guess).hexdigest()
+
+        return guess_hash[:DIFFICULTY] == "0" * DIFFICULTY
 
 
 # Instantiate our Node
@@ -114,11 +129,14 @@ blockchain = Blockchain()
 @app.route('/mine', methods=['GET'])
 def mine():
     # Run the proof of work algorithm to get the next proof
-
+    proof = blockchain.proof_of_work(blockchain.last_block)
     # Forge the new Block by adding it to the chain with the proof
-
+    previous_hash = blockchain.hash(blockchain.last_block)
+    new_block = blockchain.new_block(proof, previous_hash)
+    
+    # TODO: Send a JSON response with the new block
     response = {
-        # TODO: Send a JSON response with the new block
+        "block": new_block    
     }
 
     return jsonify(response), 200
@@ -126,8 +144,10 @@ def mine():
 
 @app.route('/chain', methods=['GET'])
 def full_chain():
+    # TODO: Return the chain and its current length
     response = {
-        # TODO: Return the chain and its current length
+        "length": len(blockchain.chain),
+        "chain": blockchain.chain
     }
     return jsonify(response), 200
 
