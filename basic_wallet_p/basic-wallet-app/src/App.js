db94b1654a7888238a27c89537421d4792fffe56
devin-bielejec <devin.bieleje@gmail.com>
2020-02-03 09:29:38 -0500

    Basic react app and got cors

--- basic_wallet_p/basic-wallet-app/src/App.js
+++ basic_wallet_p/basic-wallet-app/src/App.js
@@ -1,24 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import axios from "axios";
+
+//get transactions
+//store id which will be part of axios call
 
 function App() {
+  const [ID, setID] = React.useState("1");
+  const [transactions, setTranscations] = React.useState([]);
+
+  function handleChange(e) {
+    setID(e.target.value);
+  }
+
+  axios
+    .get(`http://localhost:5000/transactions/${ID}`)
+    .then(res => console.log(res))
+    .catch(err => console.log(err));
+
+  console.log(transactions);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wallet">
+      <form>
+        <input
+          placeholder="Put your id here"
+          value={ID}
+          onChange={handleChange}
+          type="text"
+        />
+      </form>
+
+      <h6>Amount: {}</h6>
+      <section className="transactions"></section>
     </div>
   );
 }
